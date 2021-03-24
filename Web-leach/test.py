3f3184d44c6532fe2a074fa4bd94f0ad66037915
Ratul Hasan <34002411+Ratulhasan14789@users.noreply.github.com>
2021-03-24 13:00:38 +0600

    cleaned and divided the repo
    next task is making a release

--- Web-leach/test.py
+++
@@ -1,8 +0,0 @@
-import time
-x=time.time()
-i=0
-while i<5e10:
-    i+=200
-
-print(time.time()-x)
-_= input()
--- Web-leach/unzipper.py
+++
@@ -1,5 +0,0 @@
-from zipfile import ZipFile
-from os import remove
-		
-with ZipFile('Data/.temp/'+'Web Leach v4.22.zip') as zf:
-	zf.extractall(pwd=b'lock')
