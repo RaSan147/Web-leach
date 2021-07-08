3e8a3662cb8a0bf0619c5083d113c134d7f9c615
Ratul Hasan <34002411+Ratulhasan14789@users.noreply.github.com>
2021-07-09 02:21:51 +0600

    moved backend server and recompiled

--- Web-leach_CLI/v5/leach_all_os.py
+++ Web-leach_CLI/v5/leach_all_os.py
@@ -56,7 +56,7 @@ _VERSION ="5.50004"
 #>>>>>added hash verification control in _version_update (5.500001_class)
 #>>>>>switched backend server code link from raw.git... to https://cdn.jsdelivr.net/ (see https://stackoverflow.com/questions/17341122/link-and-execute-external-javascript-file-hosted-on-github) (5.500002)
 #>>>>>DEPRICATING https://github.com/Ratulhasan14789/Web-Leach_pub/blob/main/Backend_servers/_global(v5.5+).txt
-#>>>>>NEW GLOBAL SERVER https://gitcdn.link/repo/Ratulhasan14789/Web-Leach_pub/main/Backend_servers/_global(aboveV5.5).txt
+#>>>>>NEW GLOBAL SERVER https://raw.githack.com/Ratulhasan14789/Web-Leach_pub/main/Backend_servers/_global(v5.5+).txt
 #>>>>>NEW LOCAL SERVER https://gitcdn.xyz/repo/Ratulhasan14789/Web-Leach_pub/main/Backend_servers/update%20(server%20v5.500004).txt
 #>>>>>TRYING NEW LOCAL SERVER https://raw.githack.com/Ratulhasan14789/Web-Leach_pub/main/Backend_servers/update (server v5.500004).txt
 #>>>>>Added Emojis (on title and runtime)
@@ -160,7 +160,7 @@ try:
 
 	# _server_version = "5.5"
 
-	cloud_data_link_global = 'https://gitcdn.link/repo/Ratulhasan14789/Web-Leach_pub/main/Backend_servers/_global(aboveV5.5).txt'
+	cloud_data_link_global = 'https://raw.githack.com/Ratulhasan14789/Web-Leach_pub/main/Backend_servers/_global(v5.5+).txt'
 	cloud_data_link = 'https://raw.githack.com/Ratulhasan14789/Web-Leach_pub/main/Backend_servers/update (server v5.500004).txt'
 	user_net_ip = 'offline'
 
